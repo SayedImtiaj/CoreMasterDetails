921c90c5b40d6c671150b8cc7e000d665b617d2f
Imtiajtasif <imtiajalamtawsif@gmail.com>
2024-08-27 14:35:26 +0600

    Add project files.

---
+++ Controllers/MasterDetailsController.cs
@@ -0,0 +1,120 @@
+﻿using CoreMasterDetails.Data;
+using CoreMasterDetails.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMasterDetails.Controllers
+{
+    public class MasterDetailsController : Controller
+    {
+
+        public readonly MyDbContext myDbContext;
+        public readonly IWebHostEnvironment webHost;
+
+        public MasterDetailsController(MyDbContext myDbContext, IWebHostEnvironment webHost)
+        {
+            this.myDbContext = myDbContext;
+            this.webHost = webHost;
+        }
+
+        public IActionResult Index()
+        {
+            List<Applicant> applicants;
+            applicants = myDbContext.Applicants.ToList();
+            return View(applicants);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            Applicant applicant = new Applicant();
+            applicant.Experiences.Add(new Experience() { ExperienceId = 1 });
+            return View(applicant);
+
+
+        }
+
+        [HttpPost]
+
+        public IActionResult Create(Applicant applicant)
+        {
+            applicant.Experiences.RemoveAll(n => n.YearsWorked == 0);
+            string uniqueFileName = GetUploadedFileName(applicant);
+            applicant.PhotoUrl = uniqueFileName;
+            myDbContext.Add(applicant);
+            myDbContext.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+        private string GetUploadedFileName(Applicant applicant)
+        {
+            string uniqueFileName = null;
+            if (applicant.ProfilePhoto != null)
+            {
+                string uploadFolder = Path.Combine(webHost.WebRootPath, "Images");
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + applicant.ProfilePhoto.FileName;
+                string filePath = Path.Combine(uploadFolder, uniqueFileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    applicant.ProfilePhoto.CopyTo(fileStream);
+                }
+            }
+            return uniqueFileName;
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            return View(myDbContext.Applicants.Find(id));
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Applicant applicant)
+        {
+            if (ModelState.IsValid)
+            {
+                myDbContext.Update(applicant);
+                await myDbContext.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(applicant);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Applicant applicant = myDbContext.Applicants
+                .Include(a => a.Experiences)
+                .Where(e => e.Id == id).FirstOrDefault();
+            return View(applicant);
+
+        }
+        public IActionResult Delete(int id)
+        {
+            Applicant applicant = myDbContext.Applicants
+                .Include(a => a.Experiences)
+                .Where(e => e.Id == id).FirstOrDefault();
+            return View(applicant);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Applicant applicant)
+        {
+            myDbContext.Attach(applicant);
+            myDbContext.Entry(applicant).State = EntityState.Deleted;
+            myDbContext.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
+    }
+}
